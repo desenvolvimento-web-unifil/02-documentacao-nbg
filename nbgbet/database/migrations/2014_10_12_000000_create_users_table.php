4b854738a97d5c8460ee85da381ddf2105451a4c
Brener de Lucca <brener.bernardino@edu.unifil.br>
2023-03-30 20:47:29 -0300

    Feita migration de usuário

--- nbgbet/database/migrations/2014_10_12_000000_create_users_table.php
+++ nbgbet/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 50);
+            $table->date('birth');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_adm');
+            $table->double('balance');
             $table->rememberToken();
             $table->timestamps();
         });
