093a42c067cccd3f88389106ed26559108f73923
Brener de Lucca <brener.bernardino@edu.unifil.br>
2023-06-20 22:00:39 -0300

    Iniciada implementação do deposito e do saque

---
+++ aposta/app/Http/Controllers/SaqueController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Saque;
+use App\Http\Requests\StoreSaqueRequest;
+use App\Http\Requests\UpdateSaqueRequest;
+
+class SaqueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSaqueRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Saque $saque)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Saque $saque)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSaqueRequest $request, Saque $saque)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Saque $saque)
+    {
+        //
+    }
+}
