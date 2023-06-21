0f1d39ddf2d3a8ab93ae0d6f6e68cea1592ee435
nataliak-kikuti <nataliakikuti@gmail.com>
2023-06-21 14:35:01 -0300

    tela inicial

--- aposta/resources/views/dashboard.blade.php
+++ aposta/resources/views/dashboard.blade.php
@@ -10,9 +10,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Aposte") }}
+                    {{ __("Apostas") }} 
                 </div>
             </div>
+            <button type="button" class="bg-gray-500 text-white">Criar aposta</button>
         </div>
     </div>
 </x-app-layout>
